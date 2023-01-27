38bbf115a740553fad1f6125bb3aab39ecd16781
Oleg Sidorov <sidoroffoleg@mail.ru>
2023-01-27 17:43:35 +0300

    First commit

---
+++ src/test/java/FilmManagerTest.java
@@ -0,0 +1,33 @@
+import org.junit.Assert;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class FilmManagerTest {
+
+    @Test
+    public void findAll () {
+        FilmManager manager = new FilmManager();
+
+        manager.addFilm("Film 1");
+        manager.addFilm("Film 2");
+        manager.addFilm("Film 3");
+
+        String[] expected = {"Film 1", "Film 2", "Film 3"};
+        String[] actual = manager.findAll();
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+    @Test
+    public void findlast () {
+        FilmManager manager = new FilmManager();
+
+        manager.addFilm("Film 1");
+        manager.addFilm("Film 2");
+        manager.addFilm("Film 3");
+
+        String[] expected = {"Film 3", "Film 2", "Film 1"};
+        String[] actual = manager.findLast();
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+}
